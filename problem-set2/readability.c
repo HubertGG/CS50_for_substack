cc2eb2c83b1c344bea0778daee3a6be9b59b2ef1
HubertGG <hubert.gp@hotmail.fr>
2026-07-26 20:24:02 +0200

    Update readability.c

--- problem-set2/readability.c
+++ problem-set2/readability.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
+
 #include <cs50.h>
 #include <ctype.h>
+#include <math.h>
+#include <stdio.h>
 #include <string.h>
 
 int main(void)
 {
     string text = get_string("Text: ");
-    int l = 0; //anol -> average number of letter per 100 words
-    int s = 0; // average number of sentences per 100 words
-    int now = 0; //number of words
-    int index = 0.0588 * l - 0.296 * s - 15.8;
+    double l = 0; //average number of letter per 100 words
+    double s = 0; // average number of sentences per 100 words
     int letters = 0;
     int words = 0;
     bool current_word = false;
+    int sentences = 0;
 
     for(int i = 0, n = strlen(text); i < n; i++)
     {
@@ -23,14 +24,26 @@ int main(void)
         }
         if(current_word == true && (isspace(text[i]) || ispunct(text[i])))
         {
-            words++;
-            current_word = false; // so multiple space can not be counted as word.
+            if(text[i] != '-' && text[i] !='\'')
+            {
+                words++;
+                current_word = false; // so multiple space can not be counted as word.
+                if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+                {
+                    sentences++; // number will be wrong if there is multiple punctiation like "..."
+                }
+            }
         }
     }
-    printf("%i\n", letters);
-    printf("%i\n", words);
+    l = (float)letters / words * 100;
+    s = (float)sentences / words * 100;
+    int index = round(0.0588 * l - 0.296 * s - 15.8);
 
 
-
+    printf("letters : %i\n", letters);
+    printf("words : %i\n", words);
+    printf("sentences : %i\n", sentences);
+    printf("Index : %i\n", index);
 
 }
+
